b54919a9cf5df332d583cfbdfaf6c3c1cc62e823
Gogs <karas639@gmail.com>
2021-12-13 22:54:24 +0900

    21.21.13

--- ethereum_scan.py
+++ ethereum_scan.py
@@ -2,49 +2,34 @@ import struct
 import re
 import os
 from mmap import ACCESS_READ, mmap
-import pandas as pd
-from pandas import Series, DataFrame
 
 path = input("write full path: ")
-
 try:
     handle = open(path, 'rb+')
 except IOError:
     print('IOError : File path is invalid')
     sys.exit()
-
 print(path)
 
-global df
-global count
-df = DataFrame(columns=['address'])
-count = 1000000000
-
-
-#p = re.compile(rb'0x[a-fA-F0-9]{40}$', re.DOTALL | re.IGNORECASE | re.MULTILINE)
 p = re.compile(rb'0x[a-fA-F0-9]{40}', re.IGNORECASE | re.MULTILINE)
-
-with open(path, 'rb') as f, mmap(f.fileno(), length=0, access=ACCESS_READ) as mm:
-    #print(mm[0:42])
-    b=type(mm)
-    print(b)
-        
+with open(path, 'rb') as file, mmap(file.fileno(), length=0, access=ACCESS_READ) as mm:
+    f = open('ethereum_address_scan.csv', mode='w')
+    f.write("address" + "\n")
+    f.close()
     for match in p.finditer(mm):
         s = match.start()
         e = match.end()
         grep_match_found = mm[s:e].decode("utf-8")
-        print(str(grep_match_found))
         address = str(grep_match_found)
-        for i in address:
-            y = 0
-            if y < count:
-                df = df.append(DataFrame([address], columns=['address']))
-                df.to_csv('result.csv')
-            else:
-                break
-        #g = match.group()
-        #grep_match_found = mm[g].decode("utf-8")
-        #print("match_found : ", match.group())
-
-f.close()
+        f = open('ethereum_address_scan.csv', mode='+a')
+        f.write(address + "\n")
+        print(address)
+    f.close()
+file.close()
 mm.close()
+    
+# H:\mem_dump\memdump_C_210504.mem
+# H:\mem_dump\memdump_A_210524.mem
+# /mnt/h/mem_dump/memdump_C_210504.mem
+# /mnt/h/mem_dump/memdump_A_210512.mem
+#0x9ae08d0118e105dce648a7fba6fc12b2c3e9c288
