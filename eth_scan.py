76ddda2606713c40cddb4fc14e4b7a27e06c30d7
karas639 <karas639@gmail.com>
2022-09-21 23:41:25 +0900

    update

--- eth_scan.py
+++ eth_scan.py
@@ -1,4 +1,4 @@
-# import struct
+#### import struct
 import re
 import os
 import csv
@@ -6,9 +6,12 @@ import datetime
 import requests
 import numpy as np
 from mmap import ACCESS_READ, mmap
+from web3.auto import w3
 
 now = datetime.datetime.now()
-nowDate = now.strftime('%Y-%m-%d_%H_%M_%S')
+nowDate = now.strftime('%Y-%m-%d')
+#nowDate = now.strftime('%Y-%m-%d_%H_%M_%S')
+w3.eth.account.enable_unaudited_hdwallet_features()
 
 path = input("write full path: ")
 try:
@@ -26,22 +29,20 @@ def showmenu():  # 메뉴 선택 함수
     print()
     
 def artifacts():
-    if sel == 1:
-        p = re.compile(rb'0x[a-fA-F0-9]{40}', re.IGNORECASE | re.MULTILINE) #이더리움 주소, 0x[0-9A-Fa-f]{40,44}
-    elif sel == 2:
-        p = re.compile(rb'[0-9a-f]{64}', re.IGNORECASE | re.MULTILINE) #이더리움 개인키
-    elif sel == 3:
-        p = re.compile(rb'"([address"]*)":"([a-fA-F0-9]{40})","([crypto"]*)":', re.IGNORECASE | re.MULTILINE) #이더리움 키스토어
-    elif sel == 4:
-        p = re.compile(rb'[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}', re.IGNORECASE | re.MULTILINE) # 나모닉 코드 12자리
-        mnemonic_word()
-    elif sel == 5:
-        p = re.compile(rb'[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}\s[a-z]{3,15}', re.IGNORECASE | re.MULTILINE) # 나모닉 코드 24자리
-        mnemonic_word()
+    if sel == 1: #주소, 0x[0-9A-Fa-f]{40,44}
+        p = re.compile(rb'^0x[a-fA-F0-9]{40}', re.IGNORECASE | re.MULTILINE) 
+    elif sel == 2: #개인키
+        p = re.compile(rb'[0-9a-f]{64}', re.IGNORECASE | re.MULTILINE) 
+    elif sel == 3: #키스토어
+        p = re.compile(rb'"([address"]*)":"([a-fA-F0-9]{40})","([crypto"]*)":', re.IGNORECASE | re.MULTILINE)
+    elif sel == 4: # 니모닉 코드(12)
+        p = re.compile(rb'[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}', re.MULTILINE) # 나모닉 코드 12자리
+    elif sel == 5: # 니모닉 코드(24)
+        p = re.compile(rb'[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}\s[a-z]{3,8}', re.IGNORECASE | re.MULTILINE) # 나모닉 코드 24자리
     else:
         print("another artifacts?")
+   
     print("============ Next Step ============")
-    
     with open(path, 'rb') as file, mmap(file.fileno(), length=0, access=ACCESS_READ) as mm:
         b=type(mm)
         #print(b)
@@ -50,17 +51,10 @@ def artifacts():
             e = match.end()
             grep_match_found = mm[s:e].decode("utf-8")
             match_result = str(grep_match_found) #결과값 String
-            #print(match_result)
-            with open('c:\\temp\\'+f'{nowDate}'+'_match_1차_list.txt', '+a') as file:
-                file.write(match_result+'\n')
-                file.close()
-        #mm.close()
-
-            #여기서 부터 address 한줄 씩 받아서 검증 및 중복값 제거
-            if sel == 1:
+            if sel == 1: # address는 해당 파일 사용하지 않고 결과값을 별도 csv로 파싱하여 저장(아래)
                 apikey = "WP3JPWBB21D6JRN1AVIE451N4H5YVTJN4M" 
                 URL = f"https://api.etherscan.io/api?module=account&action=txlist&apikey={apikey}&sort=desc&address={match_result}"
-                if match_result in URL:
+                if match_result in URL: # address 한줄 씩 받아서 검증 및 중복값 제거
                     global csv_file
                     csv_file = 'c:\\temp\\'+f'{nowDate}'+'_ethereum_address_scan_result.csv'
                     headers = {'User-Agent': 'Mozilla/5.0'} # header 선언, http 요청 브라우저는 Mozilla.
@@ -76,27 +70,48 @@ def artifacts():
                             to_result = api_result.get('to')
                             hx_result = api_result.get('hash')
                             csv_print()
-                        else:
-                            continue
-            elif sel == 2:
-                print("sel 2")
-            elif sel == 3:
-                print(match_result)
-            elif sel == 4:
-                mnemonic_file = open('c:\\temp\\match_1차_list.txt')
-                for line in mnemonic_file.readlines():
-                    print(line)
-                    type(line)
-                    if line in MNEM_LIST:
-                        match_mnemonic = line
-                        print("MATCH FOUND : ", match_mnemonic)
-                    #csv_file = input("file full path : ")
-                    #csv_print()
+                            break
+                continue
+            elif sel == 2: # 개인키
+                global privatekey_list
+                privatekey_list = []
+                with open('c:\\temp\\'+f'{nowDate}'+'_match_1차_list.txt', '+a') as file:
+                    file.write(match_result+'\n')
+                    privatekey_list.append(match_result)
+                    try:
+                        privatekey = w3.eth.account.privateKeyToAccount(match_result) # 개인키 입력하여 주소 확인
+                    except Exception:
+                        print("error : this is not privatekey")
                     else:
-                        #print("This is not mnemonic code")
-                        exit()
-                                
-        file.close()
+                        global privatekey_result
+                        privatekey_result = privatekey.address
+                        print(privatekey_result)
+                        privatekey_list.append(privatekey_result)
+                        csv_file = 'c:\\temp\\result_privateKey.csv'
+                        csv_print()
+                    file.close()
+            elif sel == 4 and len(match_result.split()) == 12: # 니모닉 코드(12)
+                global mnemonic_list
+                mnemonic_list = []
+                with open('c:\\temp\\'+f'{nowDate}'+'_match_1차_list.txt', '+a') as file:
+                    file.write(match_result+'\n')
+                    print("1차 검색 완료", match_result)
+                    mnemonic_list.append(match_result)
+                    try:
+                        mnemonic = w3.eth.account.from_mnemonic(match_result)  # 니모닉 코드 입력하여 주소 확인
+                    except ValidationError:
+                        print("error : this is not mnemonic")
+                    else:
+                        global mnemonic_result
+                        mnemonic_result = mnemonic.address
+                        print(mnemonic_result)
+                        mnemonic_list.append(mnemonic_result)
+                        csv_file = 'c:\\temp\\result_mnemonic.csv'
+                        csv_print()
+                    file.close()
+            elif sel == 5 and len(match_result.split()) == 24: # 니모닉 코드(24)
+                file.write(match_result+'\n')
+                file.close()
         mm.close()
         
 def csv_print():
@@ -106,17 +121,12 @@ def csv_print():
         #writer.writeheader()
         if sel == 1:
             writer.writerow([ts_result, to_result, hx_result])
+        elif sel == 2:
+            writer.writerow(privatekey_list)
         elif sel == 4:
-            writer.writerow(match_result)
+            writer.writerow(mnemonic_list)
+        csvfile.close()
 
-def mnemonic_word():
-    global MNEM_LIST
-    dicfile = open("G:\\내 드라이브\\04 - Ethereum_study\\ethereum_forensic\\mnemonic_word.csv",'r')
-    MNEMONIC_DIC = csv.reader(dicfile)
-    MNEM_LIST = list(MNEMONIC_DIC)
-    print(MNEM_LIST)
-    dicfile.close()
-    
 if __name__ == '__main__':
     menu = ("address", "private key", "keystore", "mnemonic code(12)", "mnemonic code(24)")
     while True:
@@ -139,7 +149,6 @@ if __name__ == '__main__':
         else:
             print("잘못된 메뉴 번호입니다.")
     print ("complete search")
-
 
 #0x9ae08d0118e105dce648a7fba6fc12b2c3e9c288
 # G:\내 드라이브\mem_dump\memdump.mem
