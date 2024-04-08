769c07552396e228ca8e191fc7d4c9828e08a8b6
karas639 <karas639@gmail.com>
2024-04-08 14:40:07 +0900

    Update in progress..

---
+++ ether_extract.bat
@@ -0,0 +1,53 @@
+@echo off
+cls
+title Ethereum Wallet info extract Shell
+prompt $T$S$P$G$S
+
+echo ------------------------------------------------- 
+echo          Ethereum Wallet Info Extract Kit v1.0       
+echo ------------------------------------------------- 
+
+:: -----------------------------------------------------
+:: VARIABLEs
+:: -----------------------------------------------------
+
+echo ------------------------------------------------- 
+echo       python install and pip install       
+echo ------------------------------------------------- 
+
+unzip python-3.11.9.zip -d python3
+cd python3
+curl -O https://bootstrap.pypa.io/get-pip.py
+
+echo ------------------------------------------------- 
+echo          Current Directory       
+echo ------------------------------------------------- 
+
+chdir
+
+echo ------------------------------------------------- 
+echo          Install pip packages       
+echo ------------------------------------------------- 
+
+pip install --no-index --find-links=..\requirements web3
+pip install --no-index --find-links=..\requirements eth_utils
+pip install --no-index --find-links=..\requirements numpy
+pip install --no-index --find-links=..\requirements urllib3
+pip install --no-index --find-links=..\requirements requests
+
+echo ------------------------------------------------- 
+echo          memory dump       
+echo ------------------------------------------------- 
+
+
+python ..\eth_scan_v2.py
+
+pause
+
+:: -----------------------------------------------------
+:: END
+:: -----------------------------------------------------
+:END
+echo END TIME : %DATE% %TIME% >> %_LOG%
+echo.
+echo It's done!
